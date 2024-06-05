dfb717fc85a7947127316b5ef5497e083bde12aa
ZeroX <zerox@zerosystem.ru>
2024-06-05 16:36:00 +0300

    first commit

---
+++ src/main/java/com/zerosystem/softwarestore/SoftwarestoreApplication.java
@@ -0,0 +1,13 @@
+package com.zerosystem.softwarestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwarestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerosystem/softwarestore/SoftwarestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerosystem.softwarestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwarestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
